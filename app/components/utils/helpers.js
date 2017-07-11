c76fc5df8bb03809c000a13e753185f3d52e7ddf
Michael Eggers <mdeggers84@gmail.com>
2017-07-11 15:27:38 -0400

    updated helper.js

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -1,5 +1,36 @@
 var axios = require('axios');
 
+var nytAPI = '40008c6c3f074badbf6dfed4466c83db';
+
 var helper = {
 
+  runQuery: function (search) {
+    var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+
+    queryURL += '?' + $.param({
+      q: search,
+      'api-key': nytAPI
+    });
+    return axios.get(queryURL).then(function (response) {
+      if (response.data.results[0]) {
+        return response.data.results[0].formatted;
+      }
+
+      return '';
+    });
+  },
+
+  getArticle: function () {
+    return axios.get('/api/saved');
+  },
+
+  postArticle: function (article) {
+    return axios.post('/api/saved', { article: article });
+  },
+
+  deleteArticle: function (article) {
+    return axios.delete('/api/saved', { article: article });
+  }
 };
+
+module.exports = helper;
